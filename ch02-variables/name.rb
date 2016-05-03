b0dd13ed231b6fde10f106e1c56162d19ddaf9e7
menachemkorf <menachemkorf@gmail.com>
2016-05-03 08:18:09 +0000

    Finish chapter 2 excersizes

---
+++ ch02-variables/name.rb
@@ -0,0 +1,15 @@
+# excersizes 1, 3, 4
+
+puts "Hi, what's your first name?"
+first_name = gets.chomp
+puts "And your last name?"
+last_name = gets.chomp
+full_name = "#{first_name} #{last_name}"
+greeting = "Welcome #{full_name}!"
+
+puts greeting
+
+
+10.times do
+  puts full_name
+end
